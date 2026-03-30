c81da8264a9adf613ed632f7289794714f91b360
matheusVBP <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>
2026-03-29 22:41:47 -0300

    Commit final! (Eu acho kkkk)

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-console.log(document.getElementById("cardTitle"));
+function salvarDados() {
+  localStorage.setItem("gastos", JSON.stringify(sistema.lista));
+}
+
+function carregarDados() {
+  const dados = localStorage.getItem("gastos");
+
+  if (dados) {
+    sistema.lista = JSON.parse(dados);
+  }
+}
 
 function novoGasto() {
   const card = document.getElementById("card");
@@ -6,7 +16,17 @@ function novoGasto() {
   novoGastoBtn.classList.add("oculto");
   card.classList.remove("oculto");
 
-  data.valueAsDate = new Date();
+  if (editandoId === null) {
+    data.value = new Date().toLocaleDateString("en-CA");
+  }
+
+  if (editandoId !== null) {
+    document.getElementById("cardTitle").innerText = "Editar Gasto";
+    document.getElementById("btnAdicionar").innerText = "Salvar";
+  } else {
+    document.getElementById("cardTitle").innerText = "Novo Gasto";
+    document.getElementById("btnAdicionar").innerText = "Adicionar";
+  }
 }
 
 function cancelar() {
@@ -14,6 +34,13 @@ function cancelar() {
   const novoGastoBtn = document.getElementById("novoGasto");
   card.classList.add("oculto");
   novoGastoBtn.classList.remove("oculto");
+
+  nome.value = "";
+  valor.value = "";
+  categoria.value = "";
+  data.value = "";
+
+  editandoId = null;
 }
 
 let editandoId = null;
@@ -35,12 +62,15 @@ class Gastos {
 
   adicionarGasto(gasto) {
     this.lista.push(gasto);
+    salvarDados();
   }
 
   deletarGasto(id) {
     this.lista = this.lista.filter((gasto) => gasto.id !== id);
+    salvarDados();
     atualizarStats();
     atualizarListaGastos();
+    atualizarGrafico(sistema.lista);
   }
 
   calcularTotal() {
@@ -107,6 +137,7 @@ function adicionarGasto() {
       categoria: categoria.value,
       data: data.value,
     };
+    salvarDados();
 
     editandoId = null;
   } else {
@@ -120,6 +151,7 @@ function adicionarGasto() {
   cancelar();
   atualizarStats();
   atualizarListaGastos();
+  atualizarGrafico(sistema.lista);
 }
 
 function editarGasto(id) {
@@ -132,7 +164,6 @@ function editarGasto(id) {
 
   editandoId = id;
 
-  document.getElementById("cardTitle").innerText = "✏️ Editar Gasto";
   novoGasto();
 }
 
@@ -151,24 +182,49 @@ function atualizarListaGastos(lista = sistema.listarGastos()) {
   const listaGastos = document.getElementById("listaDeGastos");
   listaGastos.innerHTML = "";
 
+  if (lista.length === 0) {
+    listaGastos.innerHTML = `
+      <div class="vazio-container">
+        <div class="vazio-icone-fundo">
+          <svg xmlns="http://www.w3.org/2000/svg" height="28px" viewBox="0 -960 960 960" width="28px" fill="#808080"><path d="M120-80v-800l60 60 60-60 60 60 60-60 60 60 60-60 60 60 60-60 60 60 60-60 60 60 60-60v800l-60-60-60 60-60-60-60 60-60-60-60 60-60-60-60 60-60-60-60 60-60-60-60 60Zm120-200h480v-80H240v80Zm0-160h480v-80H240v80Zm0-160h480v-80H240v80Zm-40 404h560v-568H200v568Zm0-568v568-568Z"/></svg>
+        </div>
+        <h3 class="vazio-titulo">Nenhum gasto encontrado</h3>
+        <p class="vazio-descricao">Adicione seu primeiro gasto ou mude os filtros</p>
+      </div>
+    `;
+    return;
+  }
+
   lista.forEach((gasto) => {
+    const coresCategoria = {
+      alimentacao: "orange",
+      transporte: "blue",
+      moradia: "violet",
+      saude: "red",
+      educacao: "cyan",
+      lazer: "yellow",
+      vestuario: "pink",
+      outros: "gray",
+    };
+
     const icone = iconesPorCategoria[gasto.categoria];
+    const cor = coresCategoria[gasto.categoria] || "gray";
     listaGastos.innerHTML += `
-        <div class="expense-item">
-  <div class="expense-icon bg-violet">${icone}</div>
-  <div class="expense-info">
-    <div class="expense-name">${gasto.nome}</div>
-    <div class="expense-meta">
-      <span class="expense-badge bg-violet text-violet">${gasto.categoria}</span>
-      <span class="expense-date">${gasto.data}</span>
-    </div>
-  </div>
-  <div class="expense-value">R$ ${gasto.valor.toFixed(2).replace(".", ",")}</div>
-  <div class="expense-actions">
-    <button onclick="editarGasto(${gasto.id})" class="icon-btn edit">✏️</button>
-    <button onclick="sistema.deletarGasto(${gasto.id})" class="icon-btn delete">🗑️</button>
-  </div>
-</div>
+      <div class="expense-item">
+        <div class="expense-icon bg-${cor}">${icone}</div>
+        <div class="expense-info">
+          <div class="expense-name">${gasto.nome}</div>
+          <div class="expense-meta">
+            <span class="expense-badge bg-${cor} text-${cor}">${gasto.categoria}</span>
+            <span class="expense-date">${gasto.data}</span>
+        </div>
+        </div>
+        <div class="expense-value">R$ ${gasto.valor.toFixed(2).replace(".", ",")}</div>
+        <div class="expense-actions">
+          <button onclick="editarGasto(${gasto.id})" class="icon-btn edit">✏️</button>
+          <button onclick="sistema.deletarGasto(${gasto.id})" class="icon-btn delete">🗑️</button>
+        </div>
+      </div>
         `;
   });
 }
@@ -274,3 +330,100 @@ btnEducacao.addEventListener("click", () => filtrarCategoria("educacao"));
 btnLazer.addEventListener("click", () => filtrarCategoria("lazer"));
 btnVestuario.addEventListener("click", () => filtrarCategoria("vestuario"));
 btnOutros.addEventListener("click", () => filtrarCategoria("outros"));
+
+let grafico;
+
+function atualizarGrafico(lista) {
+  const graficoRosquinha = document.getElementById("graficoGastos");
+  const semLista = document.getElementById("semLista");
+
+  if (grafico) {
+    grafico.destroy();
+  }
+
+  if (lista.length === 0) {
+    semLista.innerHTML =
+      "<p class='semDados'>Adicione gastos para ver o gráfico</p>";
+    return;
+  } else {
+    semLista.innerHTML = "";
+  }
+
+  const categorias = {};
+
+  lista.forEach((gastoG) => {
+    if (!categorias[gastoG.categoria]) {
+      categorias[gastoG.categoria] = 0;
+    }
+    categorias[gastoG.categoria] += gastoG.valor;
+  });
+
+  const labels = Object.keys(categorias);
+  const valores = Object.values(categorias);
+
+  const coresPorCategoria = {
+    alimentacao: "#f39c12",
+    moradia: "#8e44ad",
+    transporte: "#3498db",
+    saude: "#e74c3c",
+    educacao: "#1abc9c",
+    lazer: "#f1c40f",
+    vestuario: "#e84393",
+  };
+
+  const cores = labels.map((label) => coresPorCategoria[label] || "#ccc");
+
+  grafico = new Chart(graficoRosquinha, {
+    type: "doughnut",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Gastos por categoria",
+          data: valores,
+          backgroundColor: cores,
+          borderWidth: 0,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+
+      plugins: {
+        tooltip: {
+          callbacks: {
+            label: function (context) {
+              const nome = context.label;
+              const valor = context.raw;
+
+              const total = context.dataset.data.reduce((a, b) => a + b, 0);
+              const porcentagem = ((valor / total) * 100).toFixed(1);
+
+              return `${nome}: R$ ${valor} (${porcentagem}%)`;
+            },
+          },
+        },
+        legend: {
+          display: false,
+        },
+      },
+
+      cutout: "70%",
+    },
+  });
+}
+
+const chips = document.querySelectorAll(".chip");
+
+chips.forEach((chip) => {
+  chip.addEventListener("click", () => {
+    chips.forEach((c) => c.classList.remove("ativo"));
+    chip.classList.add("ativo");
+  });
+});
+
+carregarDados();
+atualizarListaGastos();
+atualizarStats();
+atualizarGrafico(sistema.lista);
